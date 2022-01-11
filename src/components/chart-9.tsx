87c5b48586143e8ceb076e7fbd0d6c74516afd15
berthahsu <271181958@qq.com>
2022-01-11 11:28:29 +0800

    chart14

--- src/components/chart-9.tsx
+++ src/components/chart-9.tsx
@@ -3,15 +3,16 @@ import * as echarts from 'echarts';
 import {createEchartsOptions} from '../shared/create-echarts-options';
 import {px} from '../shared/px';
 
-export const Chart4 = () => {
+export const Chart9 = () => {
   const divRef = useRef(null);
   useEffect(() => {
-    var myChart = echarts.init(divRef.current);
+    let myChart = echarts.init(divRef.current);
     myChart.setOption(createEchartsOptions({
+      color: '#F7A110',
       xAxis: {
         type: 'category',
         boundaryGap: false,
-        data: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
+        data: [0, 18, 28, 38, 48, 58, 68, 78],
         splitLine: {show: true, lineStyle: {color: '#073E78'}},
         axisTick: {show: false},
         axisLine: {show: false},
@@ -28,11 +29,9 @@ export const Chart4 = () => {
       series: [{
         type: 'line',
         data: [
-          0.15, 0.13, 0.11,
-          0.13, 0.14, 0.15,
-          0.16, 0.18, 0.21,
-          0.19, 0.17, 0.16,
-          0.15
+          0.19, 0.20, 0.26,
+          0.35, 0.26, 0.20,
+          0.08, 0.06
         ],
         symbol: 'circle',
         symbolSize: px(12),
@@ -40,10 +39,10 @@ export const Chart4 = () => {
         areaStyle: {
           color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
             offset: 0,
-            color: '#414a9f'
+            color: '#F7A110'
           }, {
             offset: 1,
-            color: '#1b1d52'
+            color: '#1B1D52'
           }]),
         }
       }]
@@ -51,9 +50,11 @@ export const Chart4 = () => {
   }, []);
 
   return (
-    <div className="bordered 案发时段">
-      <h2>案发时段分析</h2>
-      <div ref={divRef} className="chart"/>
+    <div className="年龄段-图3">
+      <h3>犯罪年龄趋势图</h3>
+      <div ref={divRef} className="chart">
+
+      </div>
     </div>
   );
 };
